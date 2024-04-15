cc4ae8e89354a2b3158343fae044c47237269160
Kishore Kumar S <livekishore2001@gmail.com>
2024-04-15 19:29:33 +0530

    Initial commit

---
+++ app/src/main/java/com/android/artflowstudio/DrawingView.kt
@@ -0,0 +1,102 @@
+package com.android.artflowstudio
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.os.Build
+import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.MotionEvent
+import android.view.View
+import androidx.annotation.RequiresApi
+
+@RequiresApi(Build.VERSION_CODES.O)
+class DrawingView(context: Context, attrs: AttributeSet): View(context, attrs) {
+    private var drawPath: CustomPath? = null
+    private var canvasBitmap: Bitmap? = null
+    private var drawPaint: Paint? = null
+    private var canvasPaint: Paint? = null
+    private var brushSize: Float = 0.toFloat()
+    private var color = Color.BLACK
+    private var canvas: Canvas? = null
+    private val paths = ArrayList<CustomPath>()
+
+    init {
+        setUpDrawing()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun setUpDrawing() {
+        drawPaint = Paint()
+        drawPath = CustomPath(color, brushSize)
+        drawPaint!!.color = color
+        drawPaint!!.style = Paint.Style.STROKE
+        drawPaint!!.strokeJoin = Paint.Join.ROUND
+        drawPaint!!.strokeCap = Paint.Cap.ROUND
+        canvasPaint = Paint(Paint.DITHER_FLAG)
+        brushSize = 20.toFloat()
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(canvasBitmap!!)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(canvasBitmap!!, 0f, 0f, canvasPaint)
+
+        // To make the drawing stay on the screen
+        for(path in paths) {
+            drawPaint!!.strokeWidth = path.brushThickness
+            drawPaint!!.color = path.color
+            canvas.drawPath(path, drawPaint!!)
+        }
+
+        if(!drawPath!!.isEmpty) {
+            drawPaint!!.strokeWidth = drawPath!!.brushThickness
+            drawPaint!!.color = drawPath!!.color
+            canvas.drawPath(drawPath!!, drawPaint!!)
+        }
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val touchX = event?.x
+        val touchY = event?.y
+
+        when(event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                drawPath!!.color = color
+                drawPath!!.brushThickness = brushSize
+
+                drawPath!!.reset()
+                drawPath!!.moveTo(touchX!!, touchY!!)
+            }
+            MotionEvent.ACTION_MOVE -> drawPath!!.lineTo(touchX!!, touchY!!)
+            MotionEvent.ACTION_UP -> {
+                paths.add(drawPath!!)
+                drawPath = CustomPath(color, brushSize)
+            }
+            else -> return false
+        }
+
+        invalidate()
+
+        return true
+    }
+
+    fun setSizeForBrush(newSize: Float) {
+        brushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, newSize, resources.displayMetrics)   // Calculates brush size in pixels based on newSize and device metrics
+        drawPaint!!.strokeWidth = brushSize
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    internal inner class CustomPath(var color: Int, var brushThickness: Float): Path()
+}
